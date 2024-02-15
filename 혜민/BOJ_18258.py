b2ff870a89f79d0c39a06ebba9ad4eb847c6fb66
hyemin hong <20200971@sungshin.ac.kr>
2024-02-15 12:37:23 +0900

    fix : 코드 리팩토링

--- 혜민/BOJ_18258.py
+++ 혜민/BOJ_18258.py
@@ -3,31 +3,24 @@ import sys
 
 queue = deque()
 n = int(input())
+ans = []
 
+# 프로세스 시작
 for _ in range(n):
-	x = sys.stdin.readline().split()    #많은 양의 데이터를 받아야해서 sys.stdin으로 input 받기
-	if x[0] == 'push':
-		queue.append(x[1])
-	elif x[0] == 'pop':
-		if queue:              #큐가 True이면 = 큐가 비어있지 않다면 
-			print(queue.popleft())
-		else:                   #큐가 False라면 = 큐가 비어있다면
-			print('-1')
-	elif x[0] == 'size':
-		print(len(queue))
-	elif x[0] == 'empty':
-		if queue:
-			print('1')
-		else:
-			print('0')
-	elif x[0] == 'front':
-		if queue:
-			print(queue[0])
-		else:
-			print('-1')
-	elif x[0] == 'back':
-		if queue:
-			print(queue[-1])
-		else:
-			print('-1')
-			
+    x = sys.stdin.readline().strip().split()
+    if x[0] == 'push':
+        queue.append(x[1])
+    elif x[0] == 'pop':
+        ans.append(queue.popleft()) if queue else ans.append('-1')
+    elif x[0] == 'size':
+        ans.append(len(queue))
+    elif x[0] == 'empty':
+        ans.append('0') if queue else ans.append('1')
+    elif x[0] == 'front':
+        ans.append(queue[0]) if queue else ans.append('-1')
+    elif x[0] == 'back':
+        ans.append(queue[-1]) if queue else ans.append('-1')
+
+# 결과 출력
+for i in ans:
+    print(i)
